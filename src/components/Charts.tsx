39208113233c5facfcc9577dc91a7ef01db9fcfe
Marco F <marco.friso@gmail.com>
2020-12-05 22:48:15 +0100

    feat: add charts

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,15 +1,79 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
+import styled from "styled-components";
+
+const LineStyle = styled.div`
+  background: white;
+  margin: 20px auto;
+  max-width: 1000px;
+  align: center;
+`;
+
+const ChartsStyle = styled.div`
+  margin-top: 40px;
+`;
+
+const setData = (chartData: Array<number>, labels: Array<number>) => ({
+  labels,
+  datasets: [
+    {
+      label: "chart",
+      data: chartData,
+      lineTension: 0,
+      fill: false,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgba(255, 99, 132, 0.2)",
+    },
+  ],
+});
+
+const setOptions = (name: string) => ({
+  title: {
+    display: true,
+    text: name,
+    fontSize: 20,
+  },
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          min: 0,
+          max: 100,
+        },
+      },
+    ],
+  },
+  legend: {
+    display: false,
+  },
+});
+
+const LineChart = ({ name, chartData, labels }: any) => (
+  <>
+    <LineStyle>
+      <Line data={setData(chartData, labels)} options={setOptions(name)} />
+    </LineStyle>
+  </>
+);
 
 const Charts = (props: any) => {
+  const len = props.chartsData[0].length;
+  const labels = [...Array(len).keys()].map((x) => x + 1);
+
   return (
-    <div>
-      CHART BELOW
-      {props.chartsData.map((chartData: any, i: number) => (
-        <div>
-          CHARTS {i} {chartData}
-        </div>
-      ))}
-    </div>
+    <>
+      <ChartsStyle>
+        {props.chartsData.map((chartData: Array<number>, i: number) => (
+          <div key={i}>
+            <LineChart
+              name={`Chart - ${i + 1}`}
+              chartData={chartData}
+              labels={labels}
+            />
+          </div>
+        ))}
+      </ChartsStyle>
+    </>
   );
 };
 
